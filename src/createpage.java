e5fc4315f37f00847ff641a1a32c495db2f4c003
RAY0514 <116180895+RAY0514@users.noreply.github.com>
2023-01-31 09:57:55 +0800

    1

--- src/createpage.java
+++ src/createpage.java
@@ -7,9 +7,6 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class createpage extends JFrame {
-
-
-
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -23,23 +20,20 @@ public class createpage extends JFrame {
         });
     }
 
-
-    JFrame frame=new JFrame();
-
+    JFrame frame = new JFrame();
     //標籤
-    JLabel IDlable= new JLabel("ID");
-    JLabel namelable= new JLabel("name");
+    JLabel IDlable = new JLabel("ID");
+    JLabel namelable = new JLabel("name");
     JLabel passwordlable = new JLabel("password");
 
     //輸入框
-    JTextField IDfield= new JTextField();
-    JTextField namefield= new JTextField();
-    JTextField passwordfield= new JTextField();
+    JTextField IDfield = new JTextField();
+    JTextField namefield = new JTextField();
+    JTextField passwordfield = new JTextField();
 
     //按鈕
     JButton resetbutton = new JButton("reset");
     JButton createbutton = new JButton("creare");
-
 
     String idfield;
     String Namefield;
@@ -48,80 +42,57 @@ public class createpage extends JFrame {
     String query;
     Statement sta;
 
-
-
-
-    public createpage(){
-
+    public createpage() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-
-        frame.setSize(500,500);
+        frame.setSize(500, 500);
         frame.setLayout(null);
         frame.setVisible(true);
-
-
-
         //標籤
-        IDlable.setBounds(50,100,75,25);
+        IDlable.setBounds(50, 100, 75, 25);
         //frame.add(IDlable);
-        namelable.setBounds(50,150,75,25);
+        namelable.setBounds(50, 150, 75, 25);
         frame.add(namelable);
-        passwordlable.setBounds(50,200,75,25);
+        passwordlable.setBounds(50, 200, 75, 25);
         frame.add(passwordlable);
-
         //輸入框
-        IDfield.setBounds(125,100,200,25);
+        IDfield.setBounds(125, 100, 200, 25);
         //frame.add(IDfield);
-        namefield.setBounds(125,150,200,25);
+        namefield.setBounds(125, 150, 200, 25);
         frame.add(namefield);
-        passwordfield.setBounds(125,200,200,25);
+        passwordfield.setBounds(125, 200, 200, 25);
         frame.add(passwordfield);
-
-
         //按鈕
-        createbutton.setBounds(125,250,100,25);
+        createbutton.setBounds(125, 250, 100, 25);
         createbutton.setFocusable(false);
         frame.add(createbutton);
         // createbutton.addActionListener();
-        resetbutton.setBounds(225,250,100,25);
+        resetbutton.setBounds(225, 250, 100, 25);
         resetbutton.setFocusable(false);
         //resetbutton.addActionListener();
         frame.add(resetbutton);
-
         resetbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ex) {
-                if (ex.getSource()==resetbutton){
+                if (ex.getSource() == resetbutton) {
                     IDfield.setText("");
                     namefield.setText("");
                     passwordfield.setText("");
-
-
                 }
-
             }
         });
-
-
-
-
 
         createbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //取得文本
-
-                idfield  = IDfield.getText();
+                idfield = IDfield.getText();
                 Namefield = namefield.getText();
                 PassWordfield = passwordfield.getText();
                 msg += "\n";
-
                 try {
                     Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/swing_demo2", "root", "123456");
-
                     query = "INSERT INTO student values('" + Namefield + "','" + PassWordfield + "' )";
-
                     sta = connection.createStatement();
                     int x = sta.executeUpdate(query);
                     if (x == 0) {
@@ -130,7 +101,7 @@ public class createpage extends JFrame {
                         JOptionPane.showMessageDialog(createbutton,
                                 "Welcome, " + msg + "Your account is sucessfully created");
                         dispose();
-                        userlogin userlogin=new userlogin();
+                        userlogin userlogin = new userlogin();
                         userlogin.setVisible(true);
                     }
                     connection.close();
@@ -139,13 +110,5 @@ public class createpage extends JFrame {
                 }
             }
         });
-
-
-
-
-
-
     }
-
-
 }
--- src/userlogin.java
+++ src/userlogin.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class userlogin extends JFrame {
-
-
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -21,14 +19,11 @@ public class userlogin extends JFrame {
         });
     }
 
-
     JFrame frame = new JFrame();
-
     //標籤
     JLabel label = new JLabel("Login");
     JLabel userlable = new JLabel("username");
     JLabel passwordlable = new JLabel("password");
-
     //輸入框
     JTextField UserField = new JTextField();
     JTextField PasswordField = new JTextField();
@@ -40,17 +35,12 @@ public class userlogin extends JFrame {
     String password;
 
     //PreparedStatement st;
-
     //ResultSet rs;
-
-
     public userlogin() {
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(500, 500);
         frame.setVisible(true);
-
         //標籤
         label.setBounds(150, 20, 100, 100);
         frame.add(label);
@@ -58,20 +48,16 @@ public class userlogin extends JFrame {
         frame.add(userlable);
         passwordlable.setBounds(50, 150, 75, 25);
         frame.add(passwordlable);
-
-
         //輸入框
         UserField.setBounds(125, 100, 200, 25);
         frame.add(UserField);
         PasswordField.setBounds(125, 150, 200, 25);
         frame.add(PasswordField);
-
         //按鈕
         loginbutton.setBounds(125, 250, 100, 25);
         frame.add(loginbutton);
         createbutton.setBounds(225, 250, 100, 25);
         frame.add(createbutton);
-
         createbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -80,28 +66,20 @@ public class userlogin extends JFrame {
                 create.setVisible(true);
             }
         });
-
-
         loginbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 username = UserField.getText();
                 password = PasswordField.getText();
-
                 try {
                     Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3307/swing_demo2",
                             "root", "123456");//連線
-
                     PreparedStatement st = (PreparedStatement) connection
                             .prepareStatement("Select name, password from student where name=? and password=?");
                     //要使用 PreparedStatement 的時候，我們的 Statement 物件就要改用 Connection 的 prepareStatement() 方法來取得
-
-
                     st.setString(1, username);
                     st.setString(2, password);
                     ResultSet rs = st.executeQuery();//
-
                     if (rs.next()) {
                         dispose();//dispose方法是關閉窗體
                         tutorial ah = new tutorial();
@@ -111,15 +89,10 @@ public class userlogin extends JFrame {
                     } else {
                         JOptionPane.showMessageDialog(loginbutton, "Wrong Username & Password");
                     }
-
-
                 } catch (SQLException sqlException) {
                     sqlException.printStackTrace();
                 }
             }
-
-
         });
-
     }
 }
